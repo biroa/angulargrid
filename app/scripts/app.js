3e2119de55558443f2189a16df0a29309dfd3b01
biroa <adam.biro@gmail.com>
2015-11-22 20:04:16 +0000

    init angular grid

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name publicHtmlApp
+ * @description
+ * # publicHtmlApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('publicHtmlApp', [
+    'ngCookies',
+    'ngSanitize',
+    'ngTouch'
+  ]);
